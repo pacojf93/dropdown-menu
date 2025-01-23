55f6369290bb46a34fc8870da13f2434f0744e7b
Paco <127772244+pacojf93@users.noreply.github.com>
2025-01-23 18:19:38 +0100

    created dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
-console.log("hello world")
+import './index.css'
 
+const dropContainer = document.querySelectorAll(".drop.container")
+const dropButton = document.querySelectorAll(".drop.button")
+
+dropContainer.forEach(dc => {
+    const button = dc.querySelector(".drop.button")
+    const list = dc.querySelector(".drop.list")
+    button.addEventListener('mouseenter',() => {
+        list.classList.remove('invisible')
+    })
+    dc.addEventListener('mouseleave',() => {
+        list.classList.add('invisible')
+    })
+})
+
+
+dropButton.forEach(db => db.addEventListener(() => {
+
+}))
