511c1d109e6c1fe878ab6101f4886e6db0b97aff
Paco <127772244+pacojf93@users.noreply.github.com>
2025-01-25 15:58:47 +0100

    created image carousel

--- src/index.js
+++ src/index.js
@@ -14,7 +14,77 @@ dropContainer.forEach(dc => {
     })
 })
 
+const carousel = document.querySelectorAll(".carousel.container")
 
-dropButton.forEach(db => db.addEventListener(() => {
+carousel.forEach(container => {
+    let selected = 0
+    
+    const width = container
+                    .attributes.getNamedItem('frame-width').value
+    const height = container
+                    .attributes.getNamedItem('frame-height').value
 
-}))
+    const frame = document.createElement('div')
+    frame.classList.add('carousel', 'frame')
+    frame.style.width = width
+    frame.style.height = height
+
+    container.parentNode.insertBefore(frame, container)
+    frame.appendChild(container)    
+    
+    const images = container
+                    .querySelectorAll(".carousel.image")
+
+    const control = document.createElement('div')
+    control.classList.add('carousel', 'control')
+
+    images.forEach((img,index) => {
+        img.style.width = width
+        img.style.height = height
+
+        const button = document.createElement('button')
+        button.classList.add('carousel', 'button')
+        if (index === 0) button.classList.add('selected')
+        button.addEventListener('click', (e) => {
+            container.style.transform = `translate(${- index * width.match(/\d+/)}px, 0px)`
+            e.currentTarget
+                .parentNode
+                .childNodes[selected]
+                .classList.remove('selected')
+            e.currentTarget.classList.add('selected')
+            selected = index
+        })
+        control.appendChild(button)        
+    })
+
+    const next = document.createElement('button')
+    next.classList.add('carousel', 'button', 'next')
+    next.textContent = '>'
+
+    next.addEventListener('click', () => {
+        const buttons = control.querySelectorAll(".carousel.button")
+        const buttonToClick = selected === images.length - 1
+                                ? 0
+                                : selected + 1
+        buttons[buttonToClick].click()
+    })
+
+    const prev = document.createElement('button')
+    prev.classList.add('carousel', 'button', 'prev')
+    prev.textContent = '<'
+
+    prev.addEventListener('click', () => {
+        const buttons = control.querySelectorAll(".carousel.button")
+        const buttonToClick = selected === 0
+                                ? images.length - 1
+                                : selected - 1
+        buttons[buttonToClick].click()
+    })
+
+    frame.appendChild(control)
+    frame.appendChild(prev)
+    frame.appendChild(next)
+
+    setInterval(() => next.click(), 5000)
+
+})
